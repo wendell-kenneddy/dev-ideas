5a46fd79d46f24d3f545c694a442780eac7aaf73
Wendell Kenneddy <wkenneddy505@gmail.com>
2022-02-18 08:22:22 -0300

    feat(app.tsx): use nprogress to show route change status

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,7 @@
+import { useEffect } from 'react';
+
 import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
 
 import { SessionProvider } from 'next-auth/react';
 
@@ -9,12 +12,31 @@ import { ToastContainer } from 'react-toastify';
 
 import { queryClient } from '../lib/queryClient';
 
+import nProgress from 'nprogress';
+
 import { Header } from '../components/Header';
 
 import '../styles/globals.css';
 import 'react-toastify/dist/ReactToastify.min.css';
+import '../styles/nprogress.css';
+
+nProgress.configure({ showSpinner: false });
 
 export default function App({ Component, pageProps }: AppProps) {
+  const { events } = useRouter();
+
+  useEffect(() => {
+    events.on('routeChangeStart', nProgress.start);
+    events.on('routeChangeComplete', nProgress.done);
+    events.on('routeChangeError', nProgress.done);
+
+    return () => {
+      events.off('routeChangeStart', nProgress.start);
+      events.off('routeChangeComplete', nProgress.done);
+      events.off('routeChangeError', nProgress.done);
+    };
+  }, [events]);
+
   return (
     <SessionProvider session={pageProps.session}>
       <QueryClientProvider client={queryClient}>
