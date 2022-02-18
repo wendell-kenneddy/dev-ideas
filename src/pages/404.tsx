9d2d52efadfac1012b7b0e701a52d26906c2e1c4
Wendell Kenneddy <wkenneddy505@gmail.com>
2022-02-18 12:05:04 -0300

    feat: add 404 error page

---
+++ src/pages/404.tsx
@@ -0,0 +1,31 @@
+import Image from 'next/image';
+
+export default function NotFound() {
+  return (
+    <main
+      className={`
+        w-[90vw]
+        max-w-[720px]
+        mx-auto
+        flex
+        flex-col
+        items-center
+        justify-center
+        gap-10
+      `}
+    >
+      <h1 className="font-medium">
+        Seems like the route could not be reached...
+      </h1>
+
+      <div className="w-[200px] h-[152px] relative">
+        <Image
+          src="/location-search.svg"
+          alt="Location search illustration"
+          layout="fill"
+          priority
+        />
+      </div>
+    </main>
+  );
+}
